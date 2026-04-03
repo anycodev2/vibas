1f2685a6f5c4b87d8954d6d262ab86496d281b07
Mushkrem <alexvsander@outlook.com>
2026-04-03 23:41:47 +0200

    basic tests setup

--- shared/Projects/SbProject.cs
+++ shared/Projects/SbProject.cs
@@ -2,6 +2,16 @@
 {
     public class SbProject
     {
+        public string Name { get; }
 
+        public SbProject(string name)
+        {
+            Name = name;
+        }
+
+        public bool HasValidName()
+        {
+            return !string.IsNullOrWhiteSpace(Name);
+        }
     }
 }
