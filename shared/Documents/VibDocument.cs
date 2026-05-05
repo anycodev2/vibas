652768a2a6ff23d6b50af1810309bea571880f84
pilarhz <p.michal18kun@gmail.com>
2026-05-05 20:51:07 +0200

    Zmiana set -> init, kontekst: zapomniałem cofnąć gdy test przeszedł

--- shared/Documents/VibDocument.cs
+++ shared/Documents/VibDocument.cs
@@ -13,7 +13,7 @@ namespace shared.Documents
         public bool isModified { get; set; }
         public string? Version { get; set; }
         public Guid Identifier { get; init; } = Guid.NewGuid();
-        public List<VibBlock> Blocks { get; set; } = new List<VibBlock>();
+        public List<VibBlock> Blocks { get; init; } = new List<VibBlock>();
         public List<VibConnection> Connections { get; set; } = new List<VibConnection>();
 
         public VibDocument(string? fileName = null, string? filePath = null, string? version = null) 
